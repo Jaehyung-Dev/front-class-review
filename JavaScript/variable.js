4448bee8e5619b8d83c88ee256c204c196482255
dev2024dev <publicdev2024@gmail.com>
2024-08-13 23:07:48 +0900

    js data type

--- JavaScript/variable.js
+++ JavaScript/variable.js
@@ -1,3 +1,5 @@
-'use strict';
-let a;
-a = 10;
+// Variable types
+// primitive, single item: number, String, boolean, null, undefined, symbol
+// object, box container => single item을 묶어서 한 단위 또는 한 박스로 관리할 수 있게 해주는 것
+// function, first-class function => function도 변수에 데이터 할당이 가능하고 함수의 파라미터로 
+// 전달할 수도 있으며 리턴 타입으로 function을 사용 가능  
